f8c3a8c8de41c04e20ed2d5cfb8fd14a68307fbf
Igor Lousada <igorcoutinho97@gmail.com>
2025-04-20 21:31:28 -0300

    do not commit message if services fail

--- app/consumer/main.go
+++ app/consumer/main.go
@@ -17,13 +17,19 @@ func (c *Consumer) Cleanup(sarama.ConsumerGroupSession) error { return nil }
 
 func (c *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 	for message := range claim.Messages() {
+		var err error
 		log.Printf("Message claimed: topic = %s, partition = %d, offset = %d, value = %s",
 			message.Topic, message.Partition, message.Offset, string(message.Value))
-			if os.Args[2] == "fanout" {
-				kafka.PublishTopic("email-topic", string(message.Value))
-			} else {
-				sendEmail(string(message.Value))
-			}
+		if os.Args[2] == "fanout" {
+			_, err = kafka.PublishTopic("email-topic", string(message.Value))
+		} else {
+			_, err = sendEmail(string(message.Value))
+		}
+
+		if err != nil {
+			log.Printf("could not process worker %s: %s", os.Args[2], os.Args[1])
+			return nil
+		}
 			
 		session.MarkMessage(message, "")
 		session.Commit()
@@ -31,8 +37,9 @@ func (c *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim saram
 	return nil
 }
 
-func sendEmail(message string) {
+func sendEmail(message string) (string, error){
 	log.Printf("sending email - %s", message)
+	return "ok", nil
 }
 
 func main() {
